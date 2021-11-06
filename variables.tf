2d8dadcc3d798b3898ed22b4800c708af6e036f3
Sean Sundberg <seansund@us.ibm.com>
2021-11-06 10:20:05 -0500

    Updates handling of provider config (#6)
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- variables.tf
+++ variables.tf
@@ -1,12 +1,3 @@
-variable "ibmcloud_api_key" {
-  type        = string
-  description = "The api key for IBM Cloud access"
-}
-
-variable "region" {
-  type        = string
-  description = "Geographic location of the resource (e.g. us-south, us-east)"
-}
 
 variable "cert_manager_id" {
   type        = string
